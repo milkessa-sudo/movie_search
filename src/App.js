1504687c0deb5b9c12dae7905d4b695546a02c2d
mekonnen <MEKONNENKENSO3@GMAIL.COM>
2021-10-01 20:58:57 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Nav from "./Nav/Nav";
 import Main from "./Main/Main";
 
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -21,3 +21,27 @@ const Nav = (props) => {
 };
 
 export default Nav;
+// //function InputField() {
+//   // State to store value from the input field
+//   const [inputValue, setInputValue] = useState("");
+
+//   // Input Field handler
+//   const handleUserInput = (e) => {
+//     setInputValue(e.target.value);
+//   };
+
+//   // Reset Input Field handler
+//   const resetInputField = () => {
+//     setInputValue("");
+//   };
+
+//   return (
+//     <div>
+//       {/* Input Field */}
+//       <input type="text" value={inputValue} onChange={handleUserInput} />
+
+//       {/* Reset button */}
+//       <button onClick={resetInputField}>Reset</button>
+//     </div>
+//   );
+// }
